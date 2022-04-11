422317ac83e0c7d2fdcc148fc43ead1f598dca89
TreOne <tre@tre.one>
2022-04-11 20:23:10 +0300

    fix(ugc_service): Избавиться от try/except в теле метода.

--- src/ugc_service/api/v1/registers.py
+++ src/ugc_service/api/v1/registers.py
@@ -1,8 +1,8 @@
 import uuid
-from http import HTTPStatus
+
+from fastapi import APIRouter, Body, Depends, Request
 
 from core import endpoints_params as ep_params
-from fastapi import APIRouter, Body, Depends, HTTPException, Request
 from models.general import Response
 from services.watcher import WatcherService, get_watcher_service
 
@@ -17,18 +17,11 @@ router = APIRouter(prefix='/registers', tags=['Регистраторы'])
 )
 async def film_watch(
     request: Request,
-    movie_id: str = Body(**ep_params.MOVIE_ID),
+    movie_id: uuid.UUID = Body(**ep_params.MOVIE_ID),
     view_ts: int = Body(**ep_params.VIEW_TS),
     watcher_service: WatcherService = Depends(get_watcher_service),
 ) -> Response:
-    try:
-        movie_id = uuid.UUID(movie_id)
-        client_id = request.state.user_uuid
-    except ValueError:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
-            detail='Badly formed hexadecimal UUID string.',
-        )
+    client_id = request.state.user_uuid
     await watcher_service.register_movie_watch(
         movie_id=movie_id, client_id=client_id, view_ts=view_ts,
     )
